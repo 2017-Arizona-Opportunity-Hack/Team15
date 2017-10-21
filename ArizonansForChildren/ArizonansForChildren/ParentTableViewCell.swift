2dc7a1f5fff79cb6cce50976fe0c5b400ea61fb9
Westin <westinrc@ymail.com>
2017-10-21 16:36:01 -0700

    Implementing initial UI.

---
+++ ArizonansForChildren/ArizonansForChildren/ParentTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ParentTableViewCell.swift
+//  ArizonansForChildren
+//
+//  Created by Westin Christensen on 10/21/17.
+//  Copyright © 2017 HackathonTeam15. All rights reserved.
+//
+
+import UIKit
+
+class ParentTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
